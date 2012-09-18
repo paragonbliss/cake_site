6d8000eaf687eb6873a83451eabf5f70f22678b1
Michael Brandt <morbidfukk@gmail.com>
2012-09-18 02:25:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CakeSite::Application.config.secret_token = 'f13da1ab267ec22136ea9fc2c6add23cc71a3fe071f3928269c94158b5b3780e588989fb2b0c4da98b1be0f1fdbd97b4b8fb59105650fd8ab1a340b022c0bbe4'
